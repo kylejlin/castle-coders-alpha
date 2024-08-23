711d98acad1c5306540085f79d1c62e87bcd0a64
Kyle Lin <kylejlin.us@gmail.com>
2024-08-23 15:47:00 -0700

    Updates

--- static/js/707.6cb81d88.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcastle_coders=self.webpackChunkcastle_coders||[]).push([[707],{2707:(e,t,s)=>{s.r(t),s.d(t,{SafeAreaWeb:()=>r});var a=s(5403);class r extends a.E_{async getSafeAreaInsets(){return{insets:{top:0,left:0,right:0,bottom:0}}}async getStatusBarHeight(){return{statusBarHeight:0}}setImmersiveNavigationBar(){throw this.unimplemented("Method not supported on Web.")}}}}]);
-//# sourceMappingURL=707.6cb81d88.chunk.js.map
---
+++ static/js/707.816bd344.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcastle_coders=self.webpackChunkcastle_coders||[]).push([[707],{707:(e,t,s)=>{s.r(t),s.d(t,{SafeAreaWeb:()=>r});var a=s(403);class r extends a.E_{async getSafeAreaInsets(){return{insets:{top:0,left:0,right:0,bottom:0}}}async getStatusBarHeight(){return{statusBarHeight:0}}setImmersiveNavigationBar(){throw this.unimplemented("Method not supported on Web.")}}}}]);
+//# sourceMappingURL=707.816bd344.chunk.js.map
